f604635bbad429401e784c0798a979df5b80e832
May <cabraloliver18@outlook.com>
2021-07-31 15:15:34 -0700

    master

--- hotmilker.py
+++ hotmilker.py
@@ -2,38 +2,25 @@ from moviepy.editor import *
 
 def hotmilkify(clip1 , clip2):
 
-    x = ImageClip(clip1).resize((640,480))
-    y = ImageClip(clip2).resize((640,480))
+    x = ImageClip(clip1).resize((800,800))
+    y = ImageClip(clip2).resize((800,800))
     hotmilk = VideoFileClip("hotmilk.mp4")
+    offset = .8
 
-    video = CompositeVideoClip(
-        [
-            hotmilk,
-            x,
+    clips = [hotmilk,x]
 
-            #
-            y.set_start(1.03),
-            x.set_start(1.96),
+    i = 0
+    while (i < 16):
+        clips.append(y.set_start(offset*(2+i)))
+        clips.append(x.set_start(offset*(3+i)))
+        i += 4
 
-            #
-            y.set_start(4.09),
-            x.set_start(5.03),
+    i = 0
+    while (i < 32):
+        clips.append(y.set_start((35+i)*(offset/2)))
+        clips.append(x.set_start((36+i)*(offset/2)))
+        i += 2
 
+    video = CompositeVideoClip(clips,size = (800,800))
 
-            y.set_start(7.15),
-            x.set_start(8.8),
-
-            y.set_start(10.21),
-            x.set_start(11.15),
-
-            y.set_start(13.27),
-            x.set_start(14.06),
-
-            y.set_start(14.21),
-            x.set_start(15)
-
-        ],
-        size = (640,470)
-    )
-
-    video.set_duration(39).write_videofile("test.mp4") 
+    video.set_duration(29).write_videofile("test.mp4", fps=20) 
