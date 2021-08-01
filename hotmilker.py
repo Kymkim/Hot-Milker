200d37bc5871e89f93ba5b308205d2381e4f8e7b
May <cabraloliver18@outlook.com>
2021-07-31 17:24:50 -0700

    fi

--- hotmilker.py
+++ hotmilker.py
@@ -1,14 +1,28 @@
 from moviepy.editor import *
 
-def hotmilkify(clip1 , clip2):
+hotmilk = VideoFileClip("hotmilk.mp4")
 
-    x = ImageClip(clip1).resize((800,800))
-    y = ImageClip(clip2).resize((800,800))
-    hotmilk = VideoFileClip("hotmilk.mp4")
-    offset = .8
+#hotmilkify(img_1, img_2, set_resizeType = 1/None, set_offset = ANY REASONABLE INT, set_fps = AGAIN, ANY REASONABLE INT, custom_dimensions = (Width, Length), output_location = File/Path/Some)
+def hotmilkify(img_1, img_2, **kwargs):
 
+    offset = kwargs.get('set_offset', .8)
+    resizeType = kwargs.get('set_resize', None)
+    sfps = kwargs.get('set_fps', 20)
+    dimensions = kwargs.get('custom_dimensions', (800,800))
+    output_location = kwargs.get('output_location', "output.mp4")
+
+    #Resize options:
+    #   1 = Squeezes the image
+    #   NONE = Literally nothing happens
+    if resizeType == 1:
+        x = ImageClip(img_1).resize((800,800))
+        y = ImageClip(img_2).resize((800,800))
+    else:
+        x = ImageClip(img_1)
+        y = ImageClip(img_2)
+
+    # Generates the image transitions
     clips = [hotmilk,x]
-
     i = 0
     while (i < 16):
         clips.append(y.set_start(offset*(2+i)))
@@ -21,6 +35,6 @@ def hotmilkify(clip1 , clip2):
         clips.append(x.set_start((36+i)*(offset/2)))
         i += 2
 
-    video = CompositeVideoClip(clips,size = (800,800))
+    video = CompositeVideoClip(clips,size = dimensions)
 
-    video.set_duration(28).write_videofile("test.mp4", fps=20) 
+    video.set_duration(28).write_videofile(output_location, fps=sfps) 
